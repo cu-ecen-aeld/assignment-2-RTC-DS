e4a864b95c8ee48f870a82d5d7c4f4e82d8d0ca3
admin <rtc-ds@Ubuntu-20-04.myguest.virtualbox.org>
2023-12-29 20:37:06 -0500

    assignment 1 finder and writer scripts

---
+++ finder-app/finder.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+#this script accepts 2 input argumnets
+#$1 - filesdir - path to a directory on file system
+#$2 - searchst - string to search within files in filesdir/*
+
+#if either of arguments are invalid then script will exit with value 1
+#if both arguments are valid then script will search for the searchstr within filesdir
+
+if [ $# -lt 2 ];then
+	echo "insufficient number of arguments"
+	exit 1
+fi
+
+if [ ! -d $1 ] ;then
+	echo $1" is not a directory in filesystem"
+	exit 1
+else
+	filesdir=$1
+fi
+if [ $2 = "" -o $2 = " " ]; then
+	echo $2" is empty"
+	exit 1
+else
+	searchstr=$2
+fi
+
+#find all files under filesdir/* and input to search of searchstr
+#echo $filesdir "\n"$searchstr
+matchfiles="$(grep -r -l $searchstr "$filesdir")" #return only files with matches
+#echo $matchfiles
+filecount=0
+matchcount=0
+increment=1
+for file in $matchfiles
+	do	
+	filecount="$(($filecount+$increment))"
+	count="$(grep -c $searchstr "$file")"
+	matchcount="$(($matchcount+$count))"	
+	#echo "FILEEE"$filecount"\t"$matchcount
+	done
+#echo $filecount
+#echo $matchcount
+echo "The number of files are "$filecount" and the number of matching lines are "$matchcount
+exit 0
+
+
---
+++ finder-app/writer.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#this script accepts 2 input argumnets
+#$1 - filepath - path to a file on file system
+#$2 - writestr - string to write in filepath
+
+#if either of arguments are invalid then script will exit with value 1
+#if both arguments are valid then script write writestr into filepath
+
+if [ $# -lt 2 ];then
+	echo "insufficient number of arguments"
+	exit 1
+fi
+
+if [ ! -f $1 ];then
+	if [ $1 = "" -o $1 = " " ]; then
+		echo $1" is empty filepath"
+		exit 1		
+	else
+		filepath=$1
+		#touch $filepath
+	fi
+else
+	filepath=$1
+fi
+
+if [ $2 = "" -o $2 = " " ]; then
+	echo $2" is empty write string"
+	exit 1
+else
+	writestr=$2
+fi
+
+#write the writestr into filepath
+mkdir -p "$(dirname "$filepath")" 
+echo $writestr >| "$filepath"
+exit 0
